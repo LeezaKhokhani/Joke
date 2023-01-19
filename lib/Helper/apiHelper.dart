2dd1e39c840cd1f4f8a6699d4ce2ba08c2f85384
khokhanileeza@gmai.com <leeza6234>
2023-01-19 20:16:36 +0530

    updated

---
+++ lib/Helper/apiHelper.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../Models/apimodels.dart';
+
+class JokesAPIHelper {
+  JokesAPIHelper._();
+
+  static final JokesAPIHelper jokesAPIHelper = JokesAPIHelper._();
+
+  Future<Jokes?> fetchJokesData() async {
+    String baseURL =
+        "https://hindi-jokes-api.onrender.com/jokes?api_key=5c01a0d89585f902ec7af10d0c60";
+
+    http.Response res = await http.get(Uri.parse(baseURL));
+
+    if (res.statusCode == 200) {
+      Map<String, dynamic> decodedData = jsonDecode(res.body);
+
+      Jokes jokes = Jokes.fromJSON(data: decodedData);
+
+      print(decodedData);
+
+      return jokes;
+    }
+    return null;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:jokeapi/screens/HomePage.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
+}
